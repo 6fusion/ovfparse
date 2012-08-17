9d82f8a7e22e49189d971455524605f3b8509c58
Kevin Olbrich <kolbrich@6fusion.com>
2012-08-17 09:38:55 -0400

    spec for parsing example ovf in fixtures

--- spec/lib/ovfparse/vmpackage_spec.rb
+++ spec/lib/ovfparse/vmpackage_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe VmPackage do
+  describe 'ourOVF.ovf' do
+    subject { VmPackage.create('file://fixtures/ourOVF.ovf').fetch }
+    it {should be_a_kind_of(FileVmPackage)}
+
+
+
+  end
+
 
 end

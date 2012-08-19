6380a41ed15ccbfddaa892ddb2deb0988ea2005d
Kevin Olbrich <kolbrich@6fusion.com>
2012-08-19 12:16:18 -0400

    specs for second vm in an OVF

--- spec/lib/ovfparse/vmpackage_spec.rb
+++ spec/lib/ovfparse/vmpackage_spec.rb
@@ -338,14 +338,134 @@ describe 'VmPackage' do
     end
 
     describe 'virtual_systems' do
-      it { should have(1).virtual_systems }
-      describe 'virtual system ComplexOVF-VMW-V8' do
-        subject { ovf.virtual_systems.first }
+      it { should have(2).virtual_systems }
+      describe 'virtual system 1' do
+        subject { ovf.virtual_systems[0] }
         its(:name) { should == "ComplexOVF-VMW-V8" }
         its(:info) { should == "A virtual machine" }
         its(:operating_system) { should == "Red Hat Enterprise Linux 6 (64-bit)" }
         its(:cpus) { should == 8 }
         its(:memory) { should == 10240 }
+        describe 'IDE Controller' do
+          it { should have(2).ide_controllers }
+          describe 'ide controller 0' do
+            subject { ovf.virtual_systems.first.ide_controllers[0] }
+            its(['Address']) { should == "1" }
+            its(['Description']) { should == "IDE Controller" }
+            its(['ElementName']) { should == "IDE 1" }
+            its(['InstanceID']) { should == "4" }
+            its(['ResourceType']) { should == "5" }
+          end
+          describe 'ide controller 1' do
+            subject { ovf.virtual_systems.first.ide_controllers[1] }
+            its(['Address']) { should == "0" }
+            its(['Description']) { should == "IDE Controller" }
+            its(['ElementName']) { should == "IDE 0" }
+            its(['InstanceID']) { should == "5" }
+            its(['ResourceType']) { should == "5" }
+          end
+        end
+
+        describe 'SCSI Controller' do
+          it { should have(1).scsi_controllers }
+          describe 'scsi controller 0' do
+            subject { ovf.virtual_systems.first.scsi_controllers.first }
+            its(['Address']) { should == "0" }
+            its(['Description']) { should == "SCSI Controller" }
+            its(['ElementName']) { should == "SCSI controller 0" }
+            its(['InstanceID']) { should == "3" }
+            its(['ResourceSubType']) { should == "VirtualSCSI" }
+            its(['ResourceType']) { should == "6" }
+          end
+        end
+
+        describe 'floppy drives' do
+          it { should have(1).floppy_drives }
+          describe 'floppy drive 1' do
+            subject { ovf.virtual_systems.first.floppy_drives.first }
+            its(['AddressOnParent']) { should == "0" }
+            its(['AutomaticAllocation']) { should == "false" }
+            its(['Description']) { should == "Floppy Drive" }
+            its(['ElementName']) { should == "Floppy drive 1" }
+            its(['InstanceID']) { should == "12" }
+            its(['ResourceType']) { should == "14" }
+          end
+        end
+
+        describe 'CD/DVD drives' do
+          it { should have(1).optical_drives }
+          describe 'optical drive 1' do
+            subject { ovf.virtual_systems.first.optical_drives.first }
+            its(['AddressOnParent']) { should == "0" }
+            its(['AutomaticAllocation']) { should == "true" }
+            its(['ElementName']) { should == "CD/DVD drive 1" }
+            its(['HostResource']) { should == "ovf:/file/file2" }
+            its(['Parent']) { should == "4" }
+            its(['InstanceID']) { should == "7" }
+            its(['ResourceType']) { should == "15" }
+          end
+        end
+
+        describe 'disks' do
+          it { should have(2).disks }
+
+        end
+        describe 'network cards' do
+          it { should have(4).network_cards }
+          describe 'network card 1' do
+            subject { ovf.virtual_systems.first.network_cards[0] }
+            its(['AddressOnParent']) { should == "7" }
+            its(['AutomaticAllocation']) { should == "true" }
+            its(['Connection']) { should == "VINET01" }
+            its(['Description']) { should == "VmxNet3 ethernet adapter on \"VINET01\"" }
+            its(['ElementName']) { should == "Network adapter 1" }
+            its(['InstanceID']) { should == "8" }
+            its(['ResourceSubType']) { should == "VmxNet3" }
+            its(['ResourceType']) { should == "10" }
+          end
+          describe 'network card 2' do
+            subject { ovf.virtual_systems.first.network_cards[1] }
+            its(['AddressOnParent']) { should == "8" }
+            its(['AutomaticAllocation']) { should == "true" }
+            its(['Connection']) { should == "VINET02" }
+            its(['Description']) { should == "E1000 ethernet adapter on \"VINET02\"" }
+            its(['ElementName']) { should == "Network adapter 2" }
+            its(['InstanceID']) { should == "9" }
+            its(['ResourceSubType']) { should == "E1000" }
+            its(['ResourceType']) { should == "10" }
+          end
+          describe 'network card 3' do
+            subject { ovf.virtual_systems.first.network_cards[2] }
+            its(['AddressOnParent']) { should == "9" }
+            its(['AutomaticAllocation']) { should == "true" }
+            its(['Connection']) { should == "VINET03" }
+            its(['Description']) { should == "VmxNet3 ethernet adapter on \"VINET03\"" }
+            its(['ElementName']) { should == "Network adapter 3" }
+            its(['InstanceID']) { should == "10" }
+            its(['ResourceSubType']) { should == "VmxNet3" }
+            its(['ResourceType']) { should == "10" }
+          end
+          describe 'network card 4' do
+            subject { ovf.virtual_systems.first.network_cards[3] }
+            its(['AddressOnParent']) { should == "10" }
+            its(['AutomaticAllocation']) { should == "true" }
+            its(['Connection']) { should == "VINET04" }
+            its(['Description']) { should == "E1000 ethernet adapter on \"VINET04\"" }
+            its(['ElementName']) { should == "Network adapter 4" }
+            its(['InstanceID']) { should == "11" }
+            its(['ResourceSubType']) { should == "E1000" }
+            its(['ResourceType']) { should == "10" }
+          end
+
+        end
+      end
+      describe 'virtual system 2' do
+        subject { ovf.virtual_systems[1] }
+        its(:name) { should == "ComplexOVF-VMW-V7" }
+        its(:info) { should == "A virtual machine" }
+        its(:operating_system) { should == "Microsoft Windows Server 2008 R2 (64-bit)" }
+        its(:cpus) { should == 4 }
+        its(:memory) { should == 4096 }
         describe 'IDE Controller' do
           it { should have(2).ide_controllers }
           describe 'ide controller 0' do
@@ -465,7 +585,7 @@ describe 'VmPackage' do
       it { should have(1).annotations }
       describe 'first annotation' do
         subject { ovf.annotations.first }
-        it { should == "Created using vSphere 5.0\nHardware Version 8" }
+        it { should == "Created by vSphere 5.0\nvApp Containing two Virtual Machines\nEach virtual machine has Version 7 and version 8 Hardware" }
       end
     end
   end

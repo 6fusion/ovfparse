8890b9534c6bd3cf0125d340e9f56d6cd3b7ea4d
Kevin Olbrich <kolbrich@6fusion.com>
2012-08-19 10:51:20 -0400

    reorganize some specs

--- spec/lib/ovfparse/vmpackage_spec.rb
+++ spec/lib/ovfparse/vmpackage_spec.rb
@@ -12,19 +12,24 @@ describe 'VmPackage' do
     its(:version) { should == "1.0" }
     its(:protocol) { should == "file" }
     #it { should be_valid }
-    its(:references) { should have(1).entries }
 
-    its(:virtual_systems) { should have(1).entries }
+    describe 'files' do
+      it { should have(1).files }
 
-    describe 'first virtual systems' do
-      subject { ovf.virtual_systems.first }
-      its(:name) { should == "MyLampService" }
-      its(:info) { should == "Single-VM Virtual appliance with LAMP stack" }
-      its(:operating_system) { should == "Linux 2.6.x" }
-      its(:cpus) { should == 1 }
-      its(:memory) { should == 256 }
-      it { should have(1).disks }
-      it { should have(1).network_cards }
+    end
+
+    describe 'virtual_systems' do
+      it { should have(1).virtual_systems }
+      describe 'first virtual systems' do
+        subject { ovf.virtual_systems.first }
+        its(:name) { should == "MyLampService" }
+        its(:info) { should == "Single-VM Virtual appliance with LAMP stack" }
+        its(:operating_system) { should == "Linux 2.6.x" }
+        its(:cpus) { should == 1 }
+        its(:memory) { should == 256 }
+        it { should have(1).disks }
+        it { should have(1).network_cards }
+      end
     end
 
   end
@@ -41,30 +46,30 @@ describe 'VmPackage' do
     its(:protocol) { should == "file" }
     #it { should be_valid }
 
-    it { should have(3).files }
     describe 'files' do
+      it { should have(3).files }
       describe 'file1' do
         subject { ovf.files[0] }
-        its(['id']) { should == "file1"}
-        its(['href']) { should == "ComplexOVF-VMW-V8-disk1.vmdk"}
-        its(['size']) { should == "69632"}
+        its(['id']) { should == "file1" }
+        its(['href']) { should == "ComplexOVF-VMW-V8-disk1.vmdk" }
+        its(['size']) { should == "69632" }
       end
       describe 'file2' do
         subject { ovf.files[1] }
-        its(['id']) { should == "file2"}
-        its(['href']) { should == "ComplexOVF-VMW-V8-file1.iso"}
-        its(['size']) { should == "203423744"}
+        its(['id']) { should == "file2" }
+        its(['href']) { should == "ComplexOVF-VMW-V8-file1.iso" }
+        its(['size']) { should == "203423744" }
       end
       describe 'file3' do
         subject { ovf.files[2] }
-        its(['id']) { should == "file3"}
-        its(['href']) { should == "ComplexOVF-VMW-V8-disk2.vmdk"}
-        its(['size']) { should == "70144"}
+        its(['id']) { should == "file3" }
+        its(['href']) { should == "ComplexOVF-VMW-V8-disk2.vmdk" }
+        its(['size']) { should == "70144" }
       end
     end
 
-    it { should have(2).disks }
     describe 'disks' do
+      it { should have(2).disks }
       describe 'vmdisk1' do
         subject { ovf.disks.first }
         its(['name']) { should == "vmdisk1" }
@@ -79,21 +84,29 @@ describe 'VmPackage' do
       end
     end
 
-    it { should have(4).networks }
+    describe 'networks' do
+      it { should have(4).networks }
 
-    it { should have(1).virtual_systems }
-    describe 'virtual system ComplexOVF-VMW-V8' do
-      subject { ovf.virtual_systems.first }
-      its(:name) { should == "ComplexOVF-VMW-V8" }
-      its(:info) { should == "A virtual machine" }
-      its(:operating_system) { should == "Red Hat Enterprise Linux 6 (64-bit)" }
-      its(:cpus) { should == 8 }
-      its(:memory) { should == 10240 }
-      it { should have(2).disks }
-      it { should have(4).network_cards }
     end
 
-    it { should have(1).annotations }
+    describe 'virtual_systems' do
+      it { should have(1).virtual_systems }
+      describe 'virtual system ComplexOVF-VMW-V8' do
+        subject { ovf.virtual_systems.first }
+        its(:name) { should == "ComplexOVF-VMW-V8" }
+        its(:info) { should == "A virtual machine" }
+        its(:operating_system) { should == "Red Hat Enterprise Linux 6 (64-bit)" }
+        its(:cpus) { should == 8 }
+        its(:memory) { should == 10240 }
+        it { should have(2).disks }
+        it { should have(4).network_cards }
+      end
+    end
+
+    describe 'annotations' do
+      it { should have(1).annotations }
+
+    end
 
   end
 
